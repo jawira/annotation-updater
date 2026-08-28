9507ee5f0aecdef29d4b8671234d0953adf03ee7
Jawira Portugal <dev@tugal.be>
2026-08-28 12:20:05 +0200

    test: Create tests

---
+++ tests/AnnotationUpdaterTest.php
@@ -0,0 +1,225 @@
+<?php declare(strict_types=1);
+
+namespace Jawira\AnnotationUpdaterTests;
+
+use Jawira\AnnotationUpdater\AnnotationUpdater;
+use PhpCsFixer\Tokenizer\Tokens;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use SplFileInfo;
+
+#[CoversClass(AnnotationUpdater::class)]
+final class AnnotationUpdaterTest extends CsTestCase
+{
+  public function testReplacesExistingAnnotationValue(): void
+  {
+    $code = <<<'PHP'
+      <?php
+      /**
+       * Some description.
+       *
+       * @copyright 2024 John Connor
+       */
+      class Foo
+      {
+      }
+      PHP;
+
+    $expected = <<<'PHP'
+      <?php
+      /**
+       * Some description.
+       *
+       * @copyright 2026 John Connor
+       */
+      class Foo
+      {
+      }
+      PHP;
+
+    $config = [
+      'annotations' => [
+        ['tag' => 'copyright', 'value' => '2026 John Connor', 'mode' => 'replace'],
+      ],
+    ];
+    $actual = $this->generateCode($code, $config);
+    $this->assertSame($expected, $actual);
+  }
+
+  public function testReplaceCollapsesDuplicateTagsIntoSingleValue(): void
+  {
+    $code = <<<'PHP'
+      <?php
+      /**
+       * Some description.
+       *
+       * @copyright 2024 John Connor
+       * @copyright 2025 John Connor
+       * @copyright 2025 John Connor
+       * @copyright 2025 John Connor
+       */
+      class Foo
+      {
+      }
+      PHP;
+
+    $expected = <<<'PHP'
+      <?php
+      /**
+       * Some description.
+       *
+       * @copyright 2026 John Connor
+       */
+      class Foo
+      {
+      }
+      PHP;
+
+    $config = [
+      'annotations' => [
+        ['tag' => 'copyright', 'value' => '2026 John Connor', 'mode' => 'replace'],
+      ],
+    ];
+    $actual = $this->generateCode($code, $config);
+    $this->assertSame($expected, $actual);
+  }
+
+  public function testPreserveDoesNotChangeExistingAnnotation(): void
+  {
+    $code = <<<'PHP'
+      <?php
+      /**
+       * Some description.
+       *
+       * @copyright 2024 John Connor
+       */
+      class Foo
+      {
+      }
+      PHP;
+
+    $config = [
+      'annotations' => [
+        ['tag' => 'copyright', 'value' => '2026 John Connor', 'mode' => 'preserve'],
+      ],
+    ];
+    $actual = $this->generateCode($code, $config);
+    $this->assertSame($actual, $actual);
+  }
+
+  public function testPreserveAddsMissingAnnotation(): void
+  {
+    $code = <<<'PHP'
+      <?php
+      /**
+       * Some description.
+       */
+      class Foo
+      {
+      }
+      PHP;
+
+    $expected = <<<'PHP'
+      <?php
+      /**
+       * Some description.
+       * @copyright 2026 John Connor
+       */
+      class Foo
+      {
+      }
+      PHP;
+
+    $config = [
+      'annotations' => [
+        ['tag' => 'copyright', 'value' => '2026 John Connor', 'mode' => 'preserve'],
+      ],
+    ];
+    $actual = $this->generateCode($code, $config);
+    $this->assertSame($expected, $actual);
+  }
+
+  public function testRemovesExistingAnnotation(): void
+  {
+    $code = <<<'PHP'
+      <?php
+      /**
+       * Some description.
+       *
+       * @copyright 1991 John Connor
+       * @copyright 1991 Sarah Connor
+       */
+      class Foo
+      {
+      }
+      PHP;
+
+    $expected = <<<'PHP'
+      <?php
+      /**
+       * Some description.
+       *
+       */
+      class Foo
+      {
+      }
+      PHP;
+
+    $config = [
+      'annotations' => [
+        ['tag' => 'copyright', 'mode' => 'remove'],
+      ],
+    ];
+    $actual = $this->generateCode($code, $config);
+    $this->assertSame($expected, $actual);
+  }
+
+  public function testAddsAnnotationWhenMissing(): void
+  {
+    $code = <<<'CODE'
+      <?php
+      /**
+       * Some description.
+       */
+      class Foo
+      {
+      }
+      CODE;
+
+    $expected = <<<'CODE'
+      <?php
+      /**
+       * Some description.
+       * @copyright 2026 John Connor
+       */
+      class Foo
+      {
+      }
+      CODE;
+
+    $config = [
+      'annotations' => [
+        ['tag' => 'copyright', 'value' => '2026 John Connor', 'mode' => 'replace'],
+      ],
+    ];
+
+    $actual = $this->generateCode($code, $config);
+
+    $this->assertSame($expected, $actual);
+  }
+
+  public function testAcceptsWrappedConfiguration(): void
+  {
+    $config = [
+      'annotations' => [
+        ['tag' => 'copyright', 'value' => '2026 John Connor', 'mode' => 'replace'],
+      ],
+    ];
+    $this->fixer->configure($config);
+
+    self::assertSame('Jawira/annotation_updater', $this->fixer->getName());
+
+  }
+
+}
+
